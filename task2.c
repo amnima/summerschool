ae7a817af78f16190016db891226d51ccf60ad01
amnima <aminmirzae@gmail.com>
2018-06-27 10:29:17 +0200

    second attempt

--- task2.c
+++ task2.c
@@ -2,20 +2,19 @@
 
 int main()
 {
-	int a = 5, b = 10;
-	int c = b/a;
-	float dummy;
+	//int a = 5, b = 10;
+	//int c = b/a;
+	int dummy;
 	//printf("The result to first expression:%d\n", c);
-	printf("Please enter a desired number\n");
-	scanf("%f",&dummy);
+	printf("Please enter your age:\n");
+	scanf("%d",&dummy);
 
-	if (type(dummy)==int){
-		printf("the number %d is an integer!\n",dummy);
+	if (dummy<24 || dummy>35){
+		printf("it is actually better for you to learn C/C++\n");
 	}
-	else if (type(dummy)==float){
-		printf("the number %f is a decimal number!\n",dummy);
+	else{
+		printf("I would suggest to go on and make a career in Fortran\n");
 	}
-
 
         // recalling getchar to hold the answer on screen 
 	getchar();
