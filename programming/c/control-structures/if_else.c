9c60d22624d485adfecceb7baceb070bc0290950
amnima <aminmirzae@gmail.com>
2018-06-27 11:10:15 +0200

    first attempt for if_else

--- programming/c/control-structures/if_else.c
+++ programming/c/control-structures/if_else.c
@@ -2,10 +2,23 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
+	//int a = 5, b = 10;
+	//int c = b/a;
+	int dummy;
+	//printf("The result to first expression:%d\n", c);
+	printf("Please enter your age:\n");
+	scanf("%d",&dummy);
 
-    // write the control structure as described in the exercise
-    // and run the code with different values for i
+	if (dummy<24 || dummy>35){
+		printf("it is actually better for you to learn C/C++\n");
+	}
+	else{
+		printf("I would suggest to go on and make a career in Fortran\n");
+	}
 
-    return 0;
+        // recalling getchar to hold the answer on screen 
+	getchar();
+       // returning 0 since we have nothing to pass to...
+       return 0;	
+	
 }
